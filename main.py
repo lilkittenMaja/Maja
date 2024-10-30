3e8c85d3850f97b8d628a081dd6332428a3cb7b7
Maja <me@madaso.ch>
2024-10-30 19:06:13 +0100

    Got Bush

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+#Simple plant loop
+# while True:
+#     if can_harvest():
+#         harvest()
+#         move(North)
+
+#Bush plant
 while True:
     if can_harvest():
         harvest()
+        plant(Entities.Bush)
         move(North)
