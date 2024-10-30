c99114eb92b8c3df7dd7d5ffd100c1f281c85b4d
Maja <me@madaso.ch>
2024-10-30 19:21:46 +0100

    CARROTS MY BELOVED

--- main.py
+++ main.py
@@ -5,8 +5,17 @@
 #         move(North)
 
 #Bush plant
+# while True:
+#     if can_harvest():
+#         harvest()
+#         plant(Entities.Bush)
+#         move(North)
+
+#Second Carrot try
 while True:
     if can_harvest():
         harvest()
-        plant(Entities.Bush)
+        till()
+        #trade(Items.Carrot_Seed)
+        plant(Entities.Carrots)
         move(North)
