fbd621073704020fabae19834eed158f6f70ed86
Maja <me@madaso.ch>
2024-11-02 07:24:37 +0100

    AAAAAAAAAAAAAAAAA

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ keep_stock("grass", 10000, farmsize)
 keep_stock("bush", 2000, farmsize)
 keep_stock("carrot", 1000, farmsize)
 keep_stock("pumpkin", 500, farmsize)
+
+#-------------------------------------------------
+#TO DO
+#-------------------------------------------------
+
+#Put tilling into it's own function
+#Till the whole field for plants that need it
+#Track if field is tilled or not
+
+#separate Harvesting from planting for pumpkins
+#To replant died pumpkins
